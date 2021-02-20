24e0c53159c9fbf7cc9e36a6d182a14b30297b3d
Safna <safna.usa@gmail.com>
2021-02-19 21:44:00 -0600

    Completed the challenge

--- java/src/main/java/com/labs1904/NextBiggestNumber.java
+++ java/src/main/java/com/labs1904/NextBiggestNumber.java
@@ -1,59 +1,56 @@
 package com.labs1904;
 
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class NextBiggestNumber {
 
     public static void main(String[] args) {
-//        Scanner in=new Scanner(System.in);
-//        System.out.println("Enter a number:  ");
-//        Integer input = in.nextInt();
-        Integer input= Integer.parseInt(args[0]);
+        Integer input = Integer.parseInt(args[0]);
         Integer nextBiggestNumber = getNextBiggestNumber(input);
         System.out.println("Input: " + input);
         System.out.println("Next biggest number: " + nextBiggestNumber);
-        }
+    }
 
-    public static int getNextBiggestNumber(Integer i) {
+    public static int getNextBiggestNumber(Integer number) {
         //TODO: IMPLEMENT ME!
+        String str = Integer.toString(number);
+        char[] array = str.toCharArray();
+        int n = array.length;
+        int result;
+        int i;
 
-              char[] array=String.valueOf(i).toCharArray();
-//              System.out.println(array);
-              int n = array.length;
-              int result;
-              int k;
-//            Checking for descending order.
-            for (k = n - 1; k > 0; k--)
-            {
-                if (array[k] > array[k - 1]) {
-//                    System.out.println("Not descending order");
-                    break;
+        // Checking for descending order.
+        for (i = n - 1; i > 0; i--) {
+            if (array[i] > array[i - 1]) {
+                break;
+            }
+        }
+        //If the number is descending order, no larger number can be created
+        if (i == 0) {
+            System.out.println("No larger number can be created");
+        }
+        // If the number is ascending order, then i+1 equals to the length.
+        else if (i + 1 == n) {
+            char temp = array[i - 1];
+            array[i - 1] = array[i];
+            array[i] = temp;
+            result = Integer.parseInt(new String(array));
+            return result;
+        } else {
+            int min = i;
+            for (int j = i + 1; j < n; j++) {
+                if (array[j] > array[i - 1] && array[j] < array[min]) {
+                    min = j;
                 }
             }
-            if (k == 0)
-            {
-                System.out.println("No larger number can be created");
-            }
-            else
-            {
-//                System.out.println("Checking conditions in else statement");
-                int min = k;
-                for (int j = k + 1; j < n; j++)
-                {
-//                    System.out.println("checking smallest number");
-                    if (array[j] > array[k-1] && array[j] < array[min])
-                    {
-                        min = j;
-                    }
-                }
-                char temp = array[k-1];
-                array[k-1] = array[min];
-                array[min] = temp;
-                Arrays.sort(array, k, n);
-                result= Integer.parseInt(new String(array));
-                return result;
-            }
-     return -1;
+            char temp = array[i - 1];
+            array[i - 1] = array[min];
+            array[min] = temp;
+            Arrays.sort(array, i, n);
+
+            result = Integer.parseInt(new String(array));
+            return result;
+        }
+        return -1;
     }
 }
